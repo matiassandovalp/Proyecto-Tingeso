ddda744e89b760791d5436a797ecf5e74d30142c
matiassandovalp <matias.sandoval.p@usach.cl>
2025-03-28 21:40:57 -0300

    Initial commit

---
+++ src/test/java/com/example/Proyecto/Tingeso/ProyectoTingesoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Proyecto.Tingeso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoTingesoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
